d66139155383206c1f3e247c39f9f7d1db619588
Ronit-gurjar <ronitgurjar999@gmail.com>
2024-12-01 02:00:09 +0530

    Add: dailyActivityPieChart component to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import DailyActivityPieChart from "@/components/daily-activity-chart";
 
 export default function Home() {
   return (
+  <main className="flex flex-col gap-4">
    <div className="font-[family-name:var(--font-geist-mono)]">
     hello, world
+
+    <Button>Click here</Button>
    </div>
+   <div>
+    <DailyActivityPieChart/>
+
+   </div>
+  </main>
   );
 }
